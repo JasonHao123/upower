d6e214fa38acd9785ca9ed78da043f59dda00c5f
Jason Hao <nk.smallbee@gmail.com>
2015-08-25 13:49:26 +0800

    asdfasdfsaf

--- weixin/src/main/java/jason/app/weixin/web/controller/HomeController.java
+++ weixin/src/main/java/jason/app/weixin/web/controller/HomeController.java
@@ -1,11 +1,24 @@
 package jason.app.weixin.web.controller;
 
+import jason.app.weixin.common.model.CreateUserCommand;
+import jason.app.weixin.social.service.ISocialService;
+
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.Session;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.core.MessageCreator;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Sample controller for going to the home page with a message
@@ -16,6 +29,12 @@ public class HomeController {
 	private static final Logger logger = LoggerFactory
 			.getLogger(HomeController.class);
 
+	@Autowired
+	private JmsTemplate jmsTemplate;
+	
+	@Autowired
+	private ISocialService socialService;
+	
 	/**
 	 * Selects the home page and populates the model with a message
 	 */
@@ -24,6 +43,23 @@ public class HomeController {
 		logger.info("Welcome home!");
 		model.addAttribute("controllerMessage",
 				"This is the message from the controller!");
+		return "home";
+	}
+	
+	@RequestMapping(value = "/jms", method = RequestMethod.GET)
+	@Transactional
+	public @ResponseBody String jms(Model model,@RequestParam("id") Long id,@RequestParam("openid") String openid) {
+		logger.info("Welcome home!");
+		model.addAttribute("controllerMessage",
+				"This is the message from the controller!");
+		final CreateUserCommand command = new CreateUserCommand(id, openid);
+		jmsTemplate.send(new MessageCreator() {
+            public Message createMessage(Session session) throws JMSException {
+              //  return session.createTextMessage("hello queue world");  
+            	logger.info("send create user command");
+            	return session.createObjectMessage(command);
+              }
+          });
 		return "home";
 	}
 	
